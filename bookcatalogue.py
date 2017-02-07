418d33d5ce5a0e074ad302dfe481d8243bac7cf8
Elizabeth <elizabeth@cs4africa.com>
2017-02-07 16:09:24 +0300

    commit

--- bookcatalogue.py
+++ bookcatalogue.py
@@ -1,51 +1,57 @@
-class SchoolLibraryCatalogue(Object):
+from datetime import date
+
+class SchoolLibraryBooks(Object):
 
 def __init__(self,name_of_book,author_of_book,book_category):
 
 	self.name_of_book = name_of_book
 	self.author_of_book = author_of_book
 	self.book_category = book_category
-	number_of_books_of_that_category = 100
 
 
-def borrow_book(self,name_of_book,date_borrowed,name_of_borrower):
+	class IssueandBorrowBooks(SchoolLibraryBooks):
 
-	if number_of_books_of_that_category>0:
+		def __init__(self,name_of_borrower,date_borrowed,date_returned):
 
-		number_of_books_of_that_category-=1;
-
-		date_borrowed = date_borrowed
-
-		name_of_borrower = name_of_borrower
-
-	else:
-
-		return 'Book is not available at the moment! Check back later'
-
-def retun_book(self,name_of_book,date_returned,name_of_returnee):
-
-     if name_of_returnee == name_of_borrower:
-
-     	number_of_books_of_that_category+=1;
-
-     	date_returned = date_returned
-
-     	name_of_returnee = name_of_returnee
+			self.name_of_borrower = name_of_borrower
+			self.date_borrowed = date_borrowed
+			self.date_returned = date_returned
+			number_of_books_of_that_category = 100
 
 
-     else:
+       def borrow_book(self,name_of_book,date_borrowed,name_of_borrower):
 
-     	return 'Special Case'
+        if number_of_books_of_that_category>0:
 
-def buy_new_books(self,num_of_new_books):
+        	number_of_books_of_that_category-=1:
 
-	number_of_books_of_that_category+=num_of_new_books
+        else:
 
-def donate_books(self,num_of_books_to_donate):
+        	return 'Book is not available'
 
-	number_of_books_of_that_category -=num_of_books_to_donate;
 
-	
+        def retun_book(self,name_of_book,date_returned,name_of_borrower):
+
+        	if abs(date_returned-date_borrowed).days > 14 days:
+
+        		number_of_books_of_that_category+=1
+
+        		return 'Pay fine'
+
+        	else:
+
+        		return 'No fine'
+
+
+        def buy_new_books(self,num_of_new_books):
+
+	        number_of_books_of_that_category+=num_of_new_books
+
+        def donate_books(self,num_of_books_to_donate):
+
+	        number_of_books_of_that_category-=num_of_books_to_donate;
+
+
 
 
 
